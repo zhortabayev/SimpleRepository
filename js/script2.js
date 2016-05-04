f8b043a64292a37ad307f2f2b1d33c7675ec0309
Nurbakyt Zhortabayev <zhortabayev@gmail.com>
2016-05-04 13:17:31 +0200

    Initial commit

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+var module = angular.module('listApp', []);
+var controller = module.controller('listController', initController);
+
+function initController($scope) {
+    $scope.items = ["First Item"];
+    $scope.addItem = function () {
+        if($scope.newItem){
+            $scope.items.push($scope.newItem);
+            $scope.newItem = undefined;
+        }    
+    }
+    
+    $scope.deleteItem = function (item) {
+        if($scope.items.lenght > 0) {
+            delete $scope.item;
+        }
+    }
+    
+}
